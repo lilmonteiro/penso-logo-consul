c249b6aec8e6ce1ee2e06a26a4fa9670648b37bf
Liandra Monteiro <liandraammonteiro@gmail.com>
2021-10-26 11:16:35 -0300

    now we can inject things on the button

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let start = function() {
-    // var botoesContainer = document.querySelector(".ui-pdp-container__row--main-actions");
-    var botoesContainer = document.querySelector(".ui-pdp--sticky-wrapper");
-    console.log(botoesContainer);
+    var botoesContainer = document.querySelector(".ui-pdp-container__row--main-actions");
+    // var botoesContainer = document.querySelector(".ui-pdp--sticky-wrapper");
+    // console.log(botoesContainer);
     // let TopLeft = getElementTopLeft(".ui-pdp-container__row--main-actions");
     // console.log(TopLeft.top + ', ' + TopLeft.left);
 
@@ -12,11 +12,17 @@ let start = function() {
     var injecao = document.createElement("div");
     injecao.classList.add("injecao");
     injecao.innerHTML = "teste";
-    botoesContainer.appendChild(injecao);
 
-    injecao.style.width = "500px";
-    injecao.style.position = "absolute !important";
-    injecao.style.zIndex= "1 !important";
+
+    injecao.style.width = "200px";
+    // injecao.style.position = "absolute !important";
+    injecao.style.zIndex = "1 !important";
+
+    var comprarAgoraBtn = document.getElementsByTagName("button")[3];
+    comprarAgoraBtn.setAttribute("id", "botao-comprar")
+    console.log(document.getElementById("botao-comprar"))
+
+    setTimeout(function() { comprarAgoraBtn.appendChild(injecao); }, 500);
 
 }
 
