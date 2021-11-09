2634afd21d6323999bbb60be5dfa9f84fd356838
Liandra Monteiro <liandraammonteiro@gmail.com>
2021-11-09 19:17:21 -0300

    Ebay Added

--- ebay-main.js
+++ ebay-main.js
@@ -11,7 +11,7 @@ let stickers = [
     "images/bem-pensado.png",
     "images/voce-realmente-quer.png",
     "images/ultima-chance.png"
-]
+];
 
 let classes = [
     "para",
@@ -26,21 +26,33 @@ let classes = [
     "pensado",
     "realmente",
     "chance"
-]
+];
+
+let hrefActions = [];
 
 let time = 30;
 
-let btnArray = document.getElementsByTagName("button");
+let btnArray = [
+    document.getElementById("binBtn_btn"),
+    document.getElementById("bidBtn_btn"),
+    document.getElementById("isCartBtn_btn"),
+    document.getElementById("vi-viewInCartBtn"),
+    document.getElementById("vi-atl-lnk-99")
+];
 
 for (const btn of btnArray) {
-    if (btn.innerHTML.includes("Comprar") || btn.innerHTML.includes("carrinho")) {
-        btn.setAttribute("formaction", " ");
-        btn.addEventListener("click", initExtension);
-    }
+    hrefActions.push(btn.getAttribute("href"));
+    btn.removeAttribute("href");
+    btn.addEventListener("click", initExtension);
+    console.log(btn);
 }
 
 function initExtension() {
 
+    let maskOly = document.querySelector(".oly_mask");
+    let modalOly = document.getElementById("vi_oly_streamLineBinOly");
+
+    let reference = document.getElementById("gh-gb");
     let pageBackground = document.createElement("div");
     let lightboxContainer = document.createElement("div");
     let lightbox = document.createElement("div");
@@ -48,13 +60,11 @@ function initExtension() {
     lightboxContainer.classList.add("lightboxContainer");
     lightbox.classList.add("lightbox");
 
-    let eRef = document.getElementById("nav-header-menu-switch");
-
     document.body.appendChild(pageBackground);
     document.body.appendChild(lightboxContainer);
     lightboxContainer.appendChild(lightbox);
 
-    document.body.insertBefore(pageBackground, eRef);
+    document.body.insertBefore(pageBackground, reference);
     document.body.insertBefore(lightboxContainer, pageBackground);
 
     lightbox.innerHTML = /*html*/ `
@@ -77,22 +87,30 @@ function initExtension() {
         </div>
         <img src=${chrome.runtime.getURL("images/logo-consul.png")}>
     </div>
-`
+    `
+    let counterEl = document.getElementById("counter");
 
-    const counterEl = document.getElementById("counter");
+    let timer = setInterval(function() {
 
-    var timer = setInterval(function() {
         time = time < 10 ? '0' + time : time;
         counterEl.innerHTML = `00:${time}`
         time--;
+
         if (time < 0) {
             clearInterval(timer);
             document.body.removeChild(pageBackground);
             document.body.removeChild(lightboxContainer);
             lightboxContainer.removeChild(lightbox);
+            for (let btnI = 0; btnI < btnArray.length; btnI++) {
+                btnArray[btnI].setAttribute("href", hrefActions[btnI]);
+                maskOly.style.display = "block !important";
+                modalOly.style.display = "block !important";
+                btnArray[btnI].removeAttribute("type");
+                btnArray[btnI].removeEventListener("click", initExtension);
+
+            }
         }
     }, 1000);
-
 
     function insertSticker(path, className) {
         let imgTag = document.createElement("img");
@@ -101,7 +119,7 @@ function initExtension() {
         lightbox.appendChild(imgTag)
     }
 
-    setTimeout(() => {
+    window.setTimeout(() => {
         for (let s = 0; s < stickers.length; s++) {
             setTimeout(function motionTimer() {
                 insertSticker(chrome.runtime.getURL(stickers[s]), ("sticker " + classes[s]));
---
+++ ml-main.js
@@ -0,0 +1,126 @@
+let stickers = [
+    "images/para.png",
+    "images/sera.png",
+    "images/respira-e-conta.png",
+    "images/1.png",
+    "images/2.png",
+    "images/3.png",
+    "images/joinha.png",
+    "images/pensou.png",
+    "images/tem-certeza.png",
+    "images/bem-pensado.png",
+    "images/voce-realmente-quer.png",
+    "images/ultima-chance.png"
+];
+
+let classes = [
+    "para",
+    "sera",
+    "respira",
+    "um",
+    "dois",
+    "tres",
+    "joinha",
+    "pensou",
+    "certeza",
+    "pensado",
+    "realmente",
+    "chance"
+];
+
+let formActions = [];
+
+let time = 30;
+
+let btnArray = document.getElementsByTagName("button");
+
+for (const btn of btnArray) {
+    if (btn.innerHTML.includes("Comprar") || btn.innerHTML.includes("carrinho")) {
+        formActions.push(btn.getAttribute("formaction"));
+        btn.removeAttribute("formaction");
+        btn.removeAttribute("type");
+        btn.setAttribute("type", "button");
+        btn.addEventListener("click", initExtension);
+    }
+}
+
+function initExtension() {
+
+    let pageBackground = document.createElement("div");
+    let lightboxContainer = document.createElement("div");
+    let lightbox = document.createElement("div");
+    pageBackground.classList.add("pageBackground");
+    lightboxContainer.classList.add("lightboxContainer");
+    lightbox.classList.add("lightbox");
+
+    let eRef = document.getElementById("nav-header-menu-switch");
+
+    document.body.appendChild(pageBackground);
+    document.body.appendChild(lightboxContainer);
+    lightboxContainer.appendChild(lightbox);
+
+    document.body.insertBefore(pageBackground, eRef);
+    document.body.insertBefore(lightboxContainer, pageBackground);
+
+    lightbox.innerHTML = /*html*/ `
+    <div class="header"> 
+        <img class="icon-black-friday" src=${chrome.runtime.getURL("images/black-friday.png")}> 
+        <p> 2021</p>
+    </div>
+
+    <div class="timer"> 
+        <p id="counter">
+        00:30
+        </p> 
+    </div>
+
+    <div class="footer">
+        <div>
+        <p class="apelo">Para</p> 
+        <p class="apelo">E</p> 
+        <p class="apelo">Pensa</p> 
+        </div>
+        <img src=${chrome.runtime.getURL("images/logo-consul.png")}>
+    </div>
+`
+    const counterEl = document.getElementById("counter");
+
+    var timer = setInterval(function() {
+
+        time = time < 10 ? '0' + time : time;
+        counterEl.innerHTML = `00:${time}`
+        time--;
+        if (time < 0) {
+            clearInterval(timer);
+            document.body.removeChild(pageBackground);
+            document.body.removeChild(lightboxContainer);
+            lightboxContainer.removeChild(lightbox);
+            for (let btnI = 0; btnI < btnArray.length; btnI++) {
+                if (btnArray[btnI].innerHTML.includes("Comprar") || btnArray[btnI].innerHTML.includes("carrinho")) {
+                    for (let actionI = 0; actionI < formActions.length; actionI++) {
+                        btnArray[btnI].setAttribute("formaction", formActions[actionI]);
+                    }
+                    btn.removeAttribute("type");
+                    btnArray[btnI].setAttribute("type", "submit");
+                    btnArray[btnI].removeEventListener("click", initExtension);
+                }
+            }
+        }
+    }, 1000);
+
+
+    function insertSticker(path, className) {
+        let imgTag = document.createElement("img");
+        imgTag.setAttribute("src", path);
+        imgTag.setAttribute("class", className);
+        lightbox.appendChild(imgTag)
+    }
+
+    setTimeout(() => {
+        for (let s = 0; s < stickers.length; s++) {
+            setTimeout(function motionTimer() {
+                insertSticker(chrome.runtime.getURL(stickers[s]), ("sticker " + classes[s]));
+            }, s * 2000);
+        }
+    }, 1000);
+}
